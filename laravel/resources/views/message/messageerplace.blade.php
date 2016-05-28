8e8dcd9cf829eabd8c2b77429bb1cf1f037ed032
keyweightdown <tonghuapian@163.com>
2016-05-28 20:07:47 +0800

    123321123321

--- laravel/resources/views/message/messageerplace.blade.php
+++ laravel/resources/views/message/messageerplace.blade.php
@@ -5,7 +5,7 @@
         <div class="userPic"><img src="{{$k->m_img}}" /></div>
         <div class="content">
             <div class="userName"><a href="javascript:;">{{$k->u_name}}</a>:</div>
-            <div class="msgInfo">{{$k->m_content}}</div>
+            <div class="msgInfo">{!!$k->m_content!!}</div>
             <div class="times"><span>{{$k->m_time}}</span>
             </div>
         </div>
