b08e61c8f8df1c6095214e143e4ae5bff51a8da7
keyweightdown <tonghuapian@163.com>
2016-05-28 20:22:29 +0800

    133321331213132132

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -1,16 +1,71 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests;
+use Session;
+use Cookie;
+use DB,Input,Redirect,url,Validator,Request;
+use App\User;
+use App\Task;
+
+/**
+ *  @U+           处理程序文件
+ *  @Home：       首页
+ *  @Author:      张龙
+ *  @Time         2016-05-17
+ */
 
 class HomeController extends Controller
 {
     public function index()
     {
-    	return view("home/index");
+    	//热销信息
+    	$selling = DB::table('house')
+    	->join('house_type','house.t_id','=','house_type.t_id')
+    	->orderBy('h_id')
+    	->where('house.is_hot','=','1')
+		->first();
+    	//热销图片
+    	$sellingImg = DB::table('images')
+		->where('images.h_id','=',$selling->h_id)
+		->first();
+
+		//精品信息
+    	$boutique = DB::table('house')
+    	->join('house_type','house.t_id','=','house_type.t_id')
+    	->orderBy('h_id')
+    	->where('house.is_hot','=','1')
+		->first();
+    	//精品图片
+    	$boutiqueImg = DB::table('images')
+		->where('images.h_id','=',$boutique->h_id)
+		->first();
+
+		//特价信息
+    	$Special = DB::table('house')
+    	->join('house_type','house.t_id','=','house_type.t_id')
+    	->orderBy('h_id')
+    	->where('house.is_cheap','=','1')
+		->first();
+    	//特价图片
+    	$SpecialImg = DB::table('images')
+		->where('images.h_id','=',$Special->h_id)
+		->first();
+
+		//特价信息
+    	$Specialoffer = DB::table('house')
+    	->join('house_type','house.t_id','=','house_type.t_id')
+    	->orderBy('h_id')
+    	->limit(1,2)
+    	->where('house.is_cheap','=','1')
+		->get();
+		
+    	//特价图片
+    	$SpecialofferImg = DB::table('images')
+		->where('images.h_id','=',$Specialoffer[0]->h_id)
+		->get();
+    	return view("home/index",array('selling'=>$selling,'sellingImg'=>$sellingImg,'boutique'=>$boutique,'boutiqueImg'=>$boutiqueImg,'Special'=>$Special,'SpecialImg'=>$SpecialImg,'Specialoffer'=>$Specialoffer,'SpecialofferImg'=>$SpecialofferImg));
     }
-    public function single()
-    {
-    	return view("home/single");
-    }
+    
     
 }
--- laravel/resources/views/home/index.blade.php
+++ laravel/resources/views/home/index.blade.php
@@ -21,15 +21,6 @@
     <!-- //animation-effect -->
     <link href='http://fonts.useso.com/css?family=Alex+Brush' rel='stylesheet' type='text/css'>
     <link href='http://fonts.useso.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link href="sel_css/css/mains.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="sel_css/js/jquery.js"></script>
-    <script type="text/javascript" src="sel_css/js/hhDrop.js"></script>
-    <script>
-      $(function(){
-        $('#hhDrop00').hhDrop({});
-        $('#hhDrop01').hhDrop({});
-      })
-    </script>
 </head>
 
 <body>
@@ -95,7 +86,7 @@
                 </script>
                 <script type="text/javascript" src="js/jquery.flexisel.js"></script>
                 <div class="more wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-                    <a href="{{URL('single')}}" class="hvr-curl-bottom-right">阅读更多</a>
+                    <a href="{{URL('perLook')}}" class="hvr-curl-bottom-right">阅读更多</a>
                 </div>
             </div>
         </div>
@@ -107,10 +98,14 @@
     <div class="container">
         <div class="banner-bottom-grids">
             <div class="col-md-5 banner-bottom-grid wow fadeInRightBig" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <h2>vel illum qui dolorem eum</h2>
-                <p>1、小区环境幽雅，美观大方，物业服务周到，邻里和睦，祥和文明，康乐和谐，业主一直自住，很爱惜房子，所以房子保持特别好，完全可以拎包入住。2、衣柜电脑桌齐全，房子装修非常的好，干净整洁，温馨舒适，照片为实景照片，真实有效</p>
+                <h2>热销房</h2>
+                <p>地址:{{$selling->h_address}}</p>
+                <p>面积:{{$selling->h_area}}平方米</p>
+                <p>可住:{{$selling->number}}人</p>
+                <p>租金:{{$selling->number}}</p>
+                <p>{!!$selling->content!!}</p>
                 <div class="more">
-                    <a href="single.html" class="hvr-curl-bottom-right">阅读更多</a>
+                    <a href="{{URL('perLook?id')}}={{$selling->h_id}}" class="hvr-curl-bottom-right">阅读更多</a>
                 </div>
             </div>
             <div class="col-md-7 banner-bottom-grid wow flipInY" data-wow-duration="1000ms" data-wow-delay="300ms">
@@ -142,149 +137,49 @@
                 <div class="banner-bottom-grid-11">
                     <img src="houst_img/2.jpg" alt=" " class="img-responsive" />
                     <div class="banner-bottom-grid-11-pos">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                            aliqua.Quis autem vel eum iure reprehenderit qui in ea voluptate
-                            velit esse quam nihil molestiae consequatur.</p>
+                        <p><br>地址:{{$boutique->h_address}}&nbsp;&nbsp;&nbsp;&nbsp;面积:{{$boutique->h_area}}平方米<br>可住:{{$boutique->number}}人&nbsp;&nbsp;&nbsp;&nbsp;租金:{{$boutique->pay}}<br>{!!$boutique->content!!}</p>
                         <div class="more m1">
-                            <a href="single.html" class="hvr-curl-bottom-right">阅读更多</a>
+                            <a href="{{URL('perLook?id')}}={{$boutique->h_id}}" class="hvr-curl-bottom-right">阅读更多</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 banner-bottom-grid-1 wow fadeInLeftBig" data-wow-duration="1500ms" data-wow-delay="100ms">
-                <h3>Excepteur sint occaecat cupidatat non proident</h3>
+                <h3>精品房</h3>
             </div>
             <div class="clearfix"> </div>
         </div>
     </div>
 </div>
 <!-- //旗下 -->
-<!-- 根据城市查询 -->
+<!-- 通讯 -->
 <div class="newsletter">
     <div class="container">
         <div class="newsletter-info">
-            <h3 class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">U+</h3>
+            <h3 class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">Newsletter</h3>
+            <p class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">But who has any right to find fault with a man who chooses to enjoy
+                a pleasure that has no annoying consequences</p>
             <form class="wow fadeInLeftBig" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <li class="thRelative" id="hhDrop00">
-                    <div class="boxSearch">
-                      <span class="key_word">
-                        <b class="size_14">北京</b>
-                        出发城市
-                      </span>
-                      <em class="ico thLeft ico_shBlue"></em>
-                      <a class="thRight ico btn_search" href="javascript:void(0)"></a>
-                    </div>
-                      
-                      <input class="thLeft" name="textfield2" type="text" id="arriveSearchText" value="请输入关键词" />
-                    <input type="button" class="ico btn_close thLeft" item="close" style="margin:8px 8px 0 0;" id="btn_delete" value="删除">
-                    <input type="submit" value="搜索">
-                      
-                    
-                    <div class="search_form_suggest" style="display:none;">
-                      <h3><a class="ico btn_close thRight" item="close">关闭</a>热门出发城市</h3>
-                      <dl class="search_hotList">
-                        <dd class="clr_after">
-                          <a href="#">北京</a>
-                          <a href="#">上海</a>
-                          <a href="#">广州</a>
-                          <a href="#">成都</a>
-                          <a href="#">杭州</a>
-                          <a href="#">南京</a>
-                          <a href="#">深圳</a>
-                          <a href="#">济南</a>
-                          <a href="#">石家庄</a>
-                          <a href="#">武汉</a>
-                          <a href="#">郑州</a>
-                          <a href="#">重庆</a>
-                          <a href="#">福州</a>
-                          <a href="#">西安</a>
-                          <a href="#">长沙</a>
-                          <a href="#">沈阳</a>
-                          <a href="#">天津</a>
-                          <a href="#">哈尔滨</a>
-                          <a href="#">苏州</a>
-                          <a href="#">南宁</a>
-                        </dd>
-                      </dl>
-                      
-                      <div class="clear"></div>
-                     
-                      
-                      <div class="thLeft thPadT5 tab_select">
-                        <dl class="clrfix">
-                          <dt>A-G</dt>
-                          <dd class="clr_after">
-                            <a href="#">北京</a>
-                            <a href="#">长沙</a>
-                            <a href="#">成都</a>
-                            <a href="#">重庆</a>
-                            <a href="#">福州</a>
-                            <a href="#">广州</a>
-                            <a href="#">贵阳</a>
-                          </dd>
-                        </dl>
-                        <dl class="clrfix">
-                          <dt>H-L</dt>
-                          <dd class="clr_after">
-                            <a href="#">哈尔滨</a>
-                            <a href="#">杭州</a>
-                            <a href="#">合肥</a>
-                            <a href="#">济南</a>
-                            <a href="#">昆明</a>
-                          </dd>
-                        </dl>
-                        <dl class="clrfix">
-                          <dt>M-T</dt>
-                          <dd class="clr_after">
-                            <a href="#">南昌</a>
-                            <a href="#">南京</a>
-                            <a href="#">南宁</a>
-                            <a href="#">宁波</a>
-                            <a href="#">青岛</a>
-                            <a href="#">三亚</a>
-                            <a href="#">上海</a>
-                            <a href="#">沈阳</a>
-                            <a href="#">深圳</a>
-                            <a href="#">石家庄</a>
-                            <a href="#">苏州</a>
-                            <a href="#">太原</a>
-                            <a href="#">天津</a>
-                          </dd>
-                        </dl>
-                        <dl class="clrfix">
-                          <dt>W-Z</dt>
-                          <dd class="clr_after">
-                            <a href="#">武汉</a>
-                            <a href="#">厦门</a>
-                            <a href="#">西安</a>
-                            <a href="#">郑州</a>
-                            <a href="#">中山</a>
-                          </dd>
-                        </dl>
-                      </div>  
-                    </div>
-                
-                    
-                </li>
-                
+                <input type="mail" value="Enter Your Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Your Email';}" required="">
+                <input type="submit" value="Send">
             </form>
         </div>
     </div>
 </div>
-<!-- //根据城市查询 -->
+<!-- //通讯 -->
 <!-- 通讯底层 -->
 <div class="newsletter-bottom">
     <div class="container">
         <div class="newsletter-bottom-grids">
             <div class="col-md-6 newsletter-bottom-grid wow fadeInLeftBig" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <h3>At vero eos et accusamus et iusto odio</h3>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                    ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                    explicabo.</p>
+                <h3>特价房</h3>
+                <p>地址:{{$Special->h_address}}</p>
+                <p>面积:{{$Special->h_area}}平方米</p>
+                <p>可住:{{$Special->number}}人</p>
+                <p>租金:{{$Special->pay}}</p>
+                <p>{!!$Special->content!!}</p>
                 <div class="more">
-                    <a href="single.html" class="hvr-curl-bottom-right">阅读更多</a>
+                    <a href="{{URL('perLook?id')}}={{$Special->h_id}}" class="hvr-curl-bottom-right">阅读更多</a>
                 </div>
             </div>
             <div class="col-md-6 newsletter-bottom-grid wow flipInY" data-wow-duration="1000ms" data-wow-delay="300ms">
@@ -297,13 +192,14 @@
                 <img src="houst_img/5.jpg" alt=" " class="img-responsive" />
             </div>
             <div class="col-md-6 newsletter-bottom-grid  wow fadeInLeftBig" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <h3>At vero eos et accusamus et iusto odio</h3>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                    ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                    explicabo.</p>
+                <h3>特价房</h3>
+                <p>地址:{{$Specialoffer[0]->h_address}}</p>
+                <p>面积:{{$Specialoffer[0]->h_area}}平方米</p>
+                <p>可住:{{$Specialoffer[0]->number}}人</p>
+                <p>可住:{{$Specialoffer[0]->pay}}</p>
+                <p>{!!$Specialoffer[0]->content!!}</p>
                 <div class="more">
-                    <a href="single.html" class="hvr-curl-bottom-right">阅读更多</a>
+                    <a href="{{URL('perLook?id')}}={{$Specialoffer[0]->h_id}}" class="hvr-curl-bottom-right">阅读更多</a>
                 </div>
             </div>
             <div class="clearfix"> </div>
