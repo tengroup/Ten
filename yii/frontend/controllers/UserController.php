71fdd865c23436e52c13331a3ee68cbcbd3b4b9c
dongmengtao <1653162261@qq.com>
2016-05-30 10:39:31 +0800

    x'c许

--- yii/frontend/controllers/UserController.php
+++ yii/frontend/controllers/UserController.php
@@ -2,30 +2,26 @@
 namespace frontend\controllers;
 use Yii;
 use yii\data\Pagination;
-use frontend\models\Users;
+use frontend\models\User;
+use yii\web\Request;
 
 /*
- *  @U+           管理中心文章处理程序文件
- *  @User：       用户管理
- *  @Author:      桃子
- *  @Time         2016-05-17
-*/
+ *   $Author: dongmegntao
+ *   $Id: UserController.php 2016-05-21 8:26 dongmengtao
+ */
+
 class UserController extends \yii\web\Controller
 {
-    /*------------------------------------------------------ */
 	//右侧
-    /*------------------------------------------------------ */
    public function actionIndex(){
-        /*  分页展示  */
         $sql1="select count(u_id) as count from users";
         $result = Yii::$app->db->createCommand($sql1);
         $m= $result->queryOne();
         //var_dump($m['count']);die;
         $num=3;
         $pagecount=ceil($m['count']/$num);
-        //echo $pagecount;die;$_GET[
-
-        isset($_GET['page'])?$page=($_GET['page']):$page=1;
+        //echo $pagecount;die;
+        isset($_GET['page'])?$page=$_GET['page']:$page=1;
         $start=($page-1)*$num;
         $sql2="select * from users limit $start,$num";
         //echo $sql2;die;
@@ -45,40 +41,49 @@ class UserController extends \yii\web\Controller
        return $this->renderPartial('yh_list.html',['lists'=>$data,'page'=>$str]);
    }
 
-    /*------------------------------------------------------ */
+
     //删除用户
-    /*------------------------------------------------------ */
     public function actionDelete(){
-        $id=$_GET['id'];
-        $del= Yii :: $app->db->createCommand()->delete('users',"u_id=$id")->execute();
-        if($del){
+        $request = Yii::$app->request;
+        $id= $request->get('id');
+        $sql= Yii :: $app->db->createCommand()->delete('users',"u_id=$id")->execute();
+        if($sql){
             return $this->redirect(['user/index']);
         }else{
             return $this->redirect(['user/index']);
         }
     }
 
-    /*------------------------------------------------------ */
     //搜索
-    /*------------------------------------------------------ */
     public function actionSearch(){
-        $search=$_GET['search'];
+        $request = Yii::$app->request;
+        $search= $request->get('search');
+      // print_r($search);die;
         $aa= Yii :: $app->db;
         $bb= $aa ->createCommand("select * from users where u_name like '%$search%'");
         $cc= $bb->queryAll();
         return $this->renderPartial('yh_lists.html',['search'=>$cc]);
     }
 
-    /*------------------------------------------------------ */
     //批删
-    /*------------------------------------------------------ */
     public function actionDeletes(){
-        $id=$_GET['id'];
-        $delAll= Yii :: $app->db->createCommand()->delete('users',"u_id in ($id)")->execute();
-        if($delAll){
+        $request = Yii::$app->request;
+        $id= $request->get('id');
+        $sql= Yii :: $app->db->createCommand()->delete('users',"u_id in ($id)")->execute();
+        if($sql){
             return $this->redirect(['user/index']);
         }else{
             return $this->redirect(['user/index']);
         }
+    }
+
+    //查看用户详细信息
+    public function actionEdit(){
+        $request = Yii::$app->request;
+        $id= $request->get('id');
+       $eval= Yii :: $app->db->createCommand("select * from users where u_id='$id'");
+       $lists=$eval->queryOne();
+       //print_r($lists);die;
+        return $this->renderPartial('yh_edit',['lists'=>$lists]);
     }
 }
